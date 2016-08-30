83fab9e4199a70d171611514d9bd567d8fb97cde
xueli.xue <xueli.xue@dianping.com>
2016-08-30 16:10:21 +0800

    重构

---
+++ xxl-mq-broker/src/test/java/NBroker.java
@@ -0,0 +1,101 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xuxueli on 16/8/29.
+ */
+public class NBroker {
+    private static Logger logger = LoggerFactory.getLogger(NBroker.class);
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                EventLoopGroup bossGroup = new NioEventLoopGroup();
+                EventLoopGroup workerGroup = new NioEventLoopGroup();
+                try {
+                    ServerBootstrap bootstrap = new ServerBootstrap();
+                    bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                            .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                                @Override
+                                public void initChannel(SocketChannel channel) throws Exception {
+                                    channel.pipeline()
+                                            .addLast(new StringDecoder())
+                                            .addLast(new StringEncoder())
+                                            .addLast(new SimpleChannelInboundHandler<String>() {
+
+
+                                                ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+                                                @Override
+                                                public void handlerAdded(ChannelHandlerContext ctx) throws Exception {  // (2)
+                                                    logger.info("handlerAdded");
+                                                    Channel incoming = ctx.channel();
+                                                    for (Channel channel : channels) {
+                                                        channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 加入\n");
+                                                    }
+                                                    channels.add(ctx.channel());
+                                                }
+
+                                                @Override
+                                                public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {  // (3)
+                                                    logger.info("handlerRemoved");
+                                                    Channel incoming = ctx.channel();
+                                                    for (Channel channel : channels) {
+                                                        channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 离开\n");
+                                                    }
+                                                    channels.remove(ctx.channel());
+                                                }
+
+                                                @Override
+                                                protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception { // (4)
+                                                    Channel incoming = ctx.channel();
+                                                    System.out.println("Broker 消息:"+s);
+
+                                                    for (Channel channel : channels) {
+                                                        if (channel != incoming){
+                                                            channel.writeAndFlush("[" + incoming.remoteAddress() + "]" + s + "\n");
+                                                        } else {
+                                                            channel.writeAndFlush("[you]" + s + "\n");
+                                                        }
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (7)
+                                                    Channel incoming = ctx.channel();
+                                                    logger.info("SimpleChatClient:"+incoming.remoteAddress()+"异常");
+                                                    // 当出现异常就关闭连接
+                                                    cause.printStackTrace();
+                                                    ctx.close();
+                                                }
+                                            });
+                                }
+                            })
+                            .option(ChannelOption.SO_BACKLOG, 128)
+                            .option(ChannelOption.TCP_NODELAY, true)
+                            .option(ChannelOption.SO_REUSEADDR, true)
+                            .childOption(ChannelOption.SO_KEEPALIVE, true);
+                    ChannelFuture future = bootstrap.bind(8080).sync();
+                    future.channel().closeFuture().sync();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    workerGroup.shutdownGracefully();
+                    bossGroup.shutdownGracefully();
+                }
+            }
+        }).start();
+    }
+}
---
+++ xxl-mq-broker/src/test/java/NPruducer.java
@@ -0,0 +1,66 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xuxueli on 16/8/29.
+ */
+public class NPruducer {
+    private static Logger logger = LoggerFactory.getLogger(NPruducer.class);
+
+    static Channel channel;
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // init channel
+                EventLoopGroup group = new NioEventLoopGroup();
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(group).channel(NioSocketChannel.class)
+                        .handler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            public void initChannel(SocketChannel channel) throws Exception {
+                                channel.pipeline()
+                                        .addLast(new StringDecoder())
+                                        .addLast(new StringEncoder())
+                                        .addLast(new SimpleChannelInboundHandler() {
+
+                                            @Override
+                                            protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                                Channel incoming = ctx.channel();
+                                                System.out.println("接受消息:"+msg);
+                                            }
+
+                                        });
+                            }
+                        })
+                        .option(ChannelOption.TCP_NODELAY, true)
+                        .option(ChannelOption.SO_REUSEADDR, true)
+                        .option(ChannelOption.SO_KEEPALIVE, true);
+                try {
+                    channel = bootstrap.connect("127.0.0.1", 8080).sync().channel();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        while (true){
+            if (channel!=null) {
+                System.out.println("发送一条消息");
+                channel.writeAndFlush("666");
+            }
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
