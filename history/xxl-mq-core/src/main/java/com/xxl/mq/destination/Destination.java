f839cdb93ce1f33bd4b40619a5ad194f52fd40df
xueli.xue <xueli.xue@dianping.com>
2016-09-09 23:06:04 +0800

    coding

--- history/xxl-mq-core/src/main/java/com/xxl/mq/destination/Destination.java
+++
@@ -1,7 +0,0 @@
-package com.xxl.mq.destination;
-
-public interface Destination {
-	enum type{
-		Topic, Queue;
-	}
-}
--- history/xxl-mq-core/src/main/java/com/xxl/mq/spring/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.xxl.mq.spring;
-
-import com.xxl.core.model.QueueMessage.StatusEnum;
-
-import java.io.Serializable;
-
-public interface MessageListener {
-	public StatusEnum onMessage(Serializable message);
-}
