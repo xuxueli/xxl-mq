4bd86572075c7b4f8ff18c9e252b6a1114b608b7
许雪里 <931591021@qq.com>
2018-11-18 21:26:17 +0800

    Update IXxlMqBroker.java

--- xxl-mq-client/src/main/java/com/xxl/mq/client/broker/remote/IXxlMqBroker.java
+++ xxl-mq-client/src/main/java/com/xxl/mq/client/broker/remote/IXxlMqBroker.java
@@ -11,7 +11,6 @@ public interface IXxlMqBroker {
 
     public int saveMessage(XxlMqMessage message);
 
-    public int saveMessageBatch(List<XxlMqMessage> messages);
 
     /**
      * 分布式获取分配给自己的数据: MOD(id, #{consumerTotal}) = #{consumerRank}, 当值为 consumerTotal>1 时生效
