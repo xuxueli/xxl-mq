22899843ff6df35257e57c37eb79a6039a847478
xuxueli <931591021@qq.com>
2018-11-20 18:58:46 +0800

    更新RM

--- xxl-mq-admin/src/main/java/com/xxl/mq/broker/controller/TopicController.java
+++ xxl-mq-admin/src/main/java/com/xxl/mq/broker/controller/TopicController.java
@@ -1,9 +1,38 @@
 package com.xxl.mq.broker.controller;
 
+import com.xxl.mq.broker.controller.annotation.PermessionLimit;
+import com.xxl.mq.broker.core.model.XxlMqBiz;
+import com.xxl.mq.broker.service.IXxlMqBizService;
+import com.xxl.mq.broker.service.IXxlMqTopicService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("/topic")
 public class TopicController {
+
+    @Resource
+    private IXxlMqTopicService xxlMqTopicService;
+    @Resource
+    private IXxlMqBizService xxlMqBizService;
+
+
+    @RequestMapping("")
+    @PermessionLimit
+    public String index(Model model){
+
+        List<XxlMqBiz> bizList = xxlMqBizService.findAll();
+        model.addAttribute("bizList", bizList);
+
+        return "topic/topic.index";
+    }
+
+    /**
+     * TODO
+     */
+
 }
